3ad5c491ad7cf1bb445b149eef3ececd18add7b8
John Tucker <jtucker@appirio.com>
2019-06-16 15:23:53 -0700

    clean

--- src/modules/my/client/client.js
+++ src/modules/my/client/client.js
@@ -4,16 +4,3 @@ const URI = 'http://localhost:5000/graphql';
 export default new ApolloClient({
     uri: URI
 });
-
-/*
-let client = null;
-export const getClient = () => {
-    if (client !== null) {
-        return client;
-    }
-    client = new ApolloClient({
-        uri: URI
-    });
-    return client;
-};
-*/
